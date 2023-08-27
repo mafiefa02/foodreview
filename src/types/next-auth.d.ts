974aa378e1a07c4598ee6c8140ce4dabf4bfca95
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2023-08-27 23:50:59 +0700

    feat: extend user session type

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -3,17 +3,14 @@ import { DefaultSession, DefaultUser } from 'next-auth';
 import { ROLE } from '@prisma/client';
 
 declare module 'next-auth' {
-  interface Session extends DefaultSession {
-    user: {
-      id: string;
-      name: string;
-      role: ROLE;
-    };
-  }
-
   interface User extends DefaultUser {
     id: string;
     name: string;
+    image: string;
     role: ROLE;
+  }
+
+  interface Session extends DefaultSession {
+    user: User;
   }
 }
